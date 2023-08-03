b018ad9e8793e63643b152d01832803f41d7b1e9
CHRISTOPHER HENDERSON <69997866+DevChris3@users.noreply.github.com>
2023-08-03 14:11:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const h1 = document.querySelector('h1');
 
 button.addEventListener('click', () => {
     const newColor = randomColor()
-    // const r = Math.floor(Math.random() * 255);
-    // const g = Math.floor(Math.random() * 255);
-    // const b = Math.floor(Math.random() * 255);
-    // const newColor = `rgb(${r}, ${g}, ${b})`;
     document.body.style.backgroundColor = newColor;
     h1.innerText = newColor;
 })
